cd86808d4d9bf1dfdc636458097049a63e2de272
Jessica Melin <jessica.sarah.melin@gmail.com>
2021-06-22 14:19:21 -0400

    Enable inputs for 4x7 segment display & comment

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,72 @@ void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+  for(int i = 2; i < 6; i++){
+    pinMode(i, OUTPUT);
+  }
 }
 void loop() {
     // take the latchPin low so
     // the LEDs don't change while you're sending in bits:
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
     digitalWrite(latchPin, LOW);
     // shift out the bits:
-    shiftOut(dataPin, clockPin, MSBFIRST, 0);
+    shiftOut(dataPin, clockPin, MSBFIRST, 249);
     //take the latch pin high so the LEDs will light up:
     digitalWrite(latchPin, HIGH);
     // pause before next value:
     delay(500);
 }
+
+/*
+
+  COMBINATIONS NEEDED TO ACHEIVE NUMBERS
+
+  First column: Resulting number or graphic
+  Second column: Bit value
+
+  ~ With decimal points ~
+  0. 64
+  1. 121
+  2. 36
+  3. 48
+  4. 25
+  5. 18
+  6. 2
+  7. 88 or 120
+  8. 0
+  9. 16 or 24
+
+  C. 70
+  c. 39
+  d. 33
+  F. 14
+  H. 9
+  h. 11
+  J. 97
+  L. 71
+  P. 12
+  y. 17
+  U. 65
+  u. 99
+
+  -. 63
+  _. 119
+
+  ~ Without decimal points ~
+  0 192
+  1 249
+  2 164
+  3 176
+  4 153
+  5 146
+  6 130
+  7 248
+  8 128
+  9 144
+
+  An interesting note: any digit or figure which is entirely lit up except for one segment will fall on a... What the heck do you call it... A primary bit number? Something like 1, 2, 4, 8, 16 etc
+*/
